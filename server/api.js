487309f53db23f109b19105b759503b7bc17b5e0
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2023-01-30 00:08:55 -0500

    step1

--- server/api.js
+++ server/api.js
@@ -8,8 +8,16 @@
 */
 
 const express = require("express");
-
 // import models so we can interact with the database
+
+var app = express()
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+
+var fs = require('fs');
+var path = require('path');
+require('dotenv/config');
+
 const User = require("./models/user");
 const Snippet = require("./models/snippet");
 const Tree = require("./models/tree");
@@ -44,6 +52,59 @@ router.post("/initsocket", (req, res) => {
 // |------------------------------|
 // | write your API methods below!|
 // |------------------------------|
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var multer = require('multer');
+ 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
+
+var imgModel = require('./models/image');
+
+app.get('/', (req, res) => {
+  imgModel.find({}, (err, items) => { 
+      if (err) {
+          console.log(err);
+          res.status(500).send('An error occurred', err);
+      }
+      else {
+          res.render('imagesPage', { items: items });
+      }
+  });
+});
+
+app.post('/', upload.single('image'), (req, res, next) => {
+ 
+  var obj = {
+      name: req.body.name,
+      desc: req.body.desc,
+      img: {
+          data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+          contentType: 'image/png'
+      }
+  }
+  imgModel.create(obj, (err, item) => {
+      if (err) {
+          console.log(err);
+      }
+      else {
+          // item.save();
+          res.redirect('/');
+      }
+  });
+});
+
+
 
 //returns an object mapping user ids to profile picture URLs
 router.get("/profile-pictures", (req, res) => {
